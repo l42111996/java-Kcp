c8d4cd6384b3ecb3c5073ceb1a7ce80adf97dca4
jinmiao <jinmiao>
2019-07-23 16:08:13 +0800

    add c# server example

---
+++ kcp-netty/src/main/java/test/Kcp4sharpExampleServer.java
@@ -0,0 +1,62 @@
+package test;
+
+import com.backblaze.erasure.fec.Snmp;
+import io.netty.buffer.ByteBuf;
+import kcp.ChannelConfig;
+import kcp.KcpListener;
+import kcp.KcpServer;
+import kcp.Ukcp;
+
+/**
+ * Created by JinMiao
+ * 2019-07-23.
+ */
+public class Kcp4sharpExampleServer implements KcpListener {
+
+    public static void main(String[] args) {
+
+        Kcp4sharpExampleServer kcpRttExampleServer = new Kcp4sharpExampleServer();
+
+        ChannelConfig channelConfig = new ChannelConfig();
+        channelConfig.setFastresend(2);
+        channelConfig.setSndwnd(300);
+        channelConfig.setRcvwnd(300);
+        channelConfig.setMtu(512);
+        channelConfig.setAckNoDelay(true);
+        channelConfig.setInterval(40);
+        channelConfig.setNocwnd(true);
+        channelConfig.setTimeoutMillis(10000);
+        channelConfig.setAutoSetConv(true);
+        //这几个参数c#端未实现
+        channelConfig.setCrc32Check(false);
+        //channelConfig.setFecDataShardCount(10);
+        //channelConfig.setFecParityShardCount(3);
+        KcpServer kcpServer = new KcpServer();
+        kcpServer.init(Runtime.getRuntime().availableProcessors(), kcpRttExampleServer,channelConfig,10009);
+    }
+
+
+    @Override
+    public void onConnected(Ukcp ukcp) {
+        System.out.println("有连接进来"+Thread.currentThread().getName()+ukcp.user().getRemoteAddress());
+    }
+
+    @Override
+    public void handleReceive(ByteBuf buf, Ukcp kcp) {
+        byte[] bytes = new  byte[buf.readableBytes()];
+        buf.getBytes(buf.readerIndex(),bytes);
+        System.out.println("收到消息: "+new String(bytes));
+        kcp.write(buf);
+    }
+
+    @Override
+    public void handleException(Throwable ex, Ukcp kcp) {
+        ex.printStackTrace();
+    }
+
+    @Override
+    public void handleClose(Ukcp kcp) {
+        System.out.println(Snmp.snmp.toString());
+        Snmp.snmp  = new Snmp();
+    }
+}
